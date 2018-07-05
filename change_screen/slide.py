89c3fd9bb6bad2a555df5e0e4a2735424b92f353
Miura55 <acordion.piano@gmail.com>
2018-07-06 01:52:27 +0900

    Get info

--- change_screen/slide.py
+++ change_screen/slide.py
@@ -9,21 +9,10 @@ from datetime import datetime
 
 
 
-class Painter(Widget):
-    def on_touch_down(self, touch):
-        with self.canvas:
-            touch.ud["line"] = Line(points=(touch.x, touch.y))
-
-    def on_touch_move(self,touch):
-        touch.ud["line"].points += [touch.x, touch.y]
-
 class MainScreen(Screen):
     def __init__(self, **kwargs):
         super(MainScreen, self).__init__(**kwargs)
-    # def on_touch_down(self, touch):
-    #     labeldatetime = ObjectProperty(None)
-    #     buttoncount = ObjectProperty(None)
-    def buttoncount_clicked(self):
+    def get_Info(self):
         now = datetime.now()
         self.now_ = now.strftime('%Y,%m,%d  %H:%M:%S')
         # self.labeldatetime.text = s1tr(self.root.now_)
@@ -42,11 +31,11 @@ class AnotherScreen(Screen):
 class ScreenManagement(ScreenManager):
     pass
 
-presentation = Builder.load_file("layout.kv")
 
 class ExperimentApp(App):
     def build(self):
         return presentation
 
 if __name__ == '__main__':
+    presentation = Builder.load_file("layout.kv")
     ExperimentApp().run()
