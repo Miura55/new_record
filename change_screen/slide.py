c2c57d3fba6b7a7c9540b28e86ab572e019446fd
Miura55 <acordion.piano@gmail.com>
2018-07-08 14:55:46 +0900

    Add graph

--- change_screen/slide.py
+++ change_screen/slide.py
@@ -6,24 +6,62 @@ from kivy.properties import ObjectProperty
 from kivy.uix.widget import Widget
 from kivy.graphics import Line
 
+from kivy.uix.boxlayout import BoxLayout
+from kivy.garden.graph import MeshLinePlot
+from kivy.clock import Clock
+from threading import Thread
+import audioop
+import pyaudio
+
+
+def get_microphone_level():
+    """
+    source: http://stackoverflow.com/questions/26478315/getting-volume-levels-from-pyaudio-for-use-in-arduino
+    audioop.max alternative to audioop.rms
+    """
+    chunk = 1024
+    FORMAT = pyaudio.paInt16
+    CHANNELS = 1
+    RATE = 44100
+    p = pyaudio.PyAudio()
+
+    s = p.open(format=FORMAT,
+               channels=CHANNELS,
+               rate=RATE,
+               input=True,
+               frames_per_buffer=chunk)
+    global levels
+    while True:
+        data = s.read(chunk)
+        mx = audioop.rms(data, 2)
+        if len(levels) >= 100:
+            levels = []
+        levels.append(mx)
+
 class get_Info(Widget):
     def on_touch_down(self, touch):
         labeldatetime = ObjectProperty(None)
         buttoncount = ObjectProperty(None)
 
-class Painter(Widget):
-    def on_touch_down(self, touch):
-        with self.canvas:
-            touch.ud["line"] = Line(points=(touch.x, touch.y))
-
-    def on_touch_move(self,touch):
-        touch.ud["line"].points += [touch.x, touch.y]
 
 class MainScreen(Screen):
     pass
 
 class AnotherScreen(Screen):
-    pass
+    def __init__(self,):
+        super(Logic, self).__init__()
+        self.plot = MeshLinePlot(color=[1, 0, 0, 1])
+
+    def start(self):
+        self.ids.graph.add_plot(self.plot)
+        Clock.schedule_interval(self.get_value, 0.001)
+
+    def stop(self):
+        Clock.unschedule(self.get_value)
+
+    def get_value(self, dt):
+        self.plot.points = [(i, j/5) for i, j in enumerate(levels)]
+
 
 class ScreenManagement(ScreenManager):
     pass
@@ -42,8 +80,12 @@ class MainApp(App):
         age_ = self.root.agetext.text
         profile = [nowtime,'\n', name_,'\n', age_]
         name_file = nowtime + '.txt'
-        with open(name_file, 'w') as f: #書き込みモードでオープン
+        with open(name_file, 'w') as f:
             f.writelines(profile)
 
 if __name__ == '__main__':
+    levels = []  # store levels of microphone
+    get_level_thread = Thread(target = get_microphone_level)
+    get_level_thread.daemon = True
+    get_level_thread.start()
     MainApp().run()
